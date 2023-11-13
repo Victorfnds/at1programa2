0192c12ddaac66cc8ff3e46d5b739650c27f9739
Victorfnds <149596001+Victorfnds@users.noreply.github.com>
2023-11-13 15:43:13 -0300

    Update at1programa2.py

--- at1programa2.py
+++ at1programa2.py
@@ -1,46 +1,30 @@
-import sys
 # menu principal
+
+import sys
 def main():
- while True:
-  print('-' * 20)
-  print('MENU')
-  print('-' * 20)
-  print()
-  print('OPÇÕES', )
-  print('[C]', - 'COMPRA')
-  print('[V]', - 'VENDA')
-  print('[S]', - 'SAIR')
-resposta = input(" Selecione sua opção:" )
-if resposta =="c":
- transacao("compra")
-elif resposta =="v":
- transacao("venda")
-elif resposta =="s":
- print('saindo do programa')
- sys.exit()
+   while True:
+    resposta = input(" Selecione sua opção:c para compra,v para venda,s para sair")
+    if resposta == "c":
+     transacao("compra")
+    elif resposta == "v":
+     transacao("venda")
+    elif resposta == "s":
+     print('saindo do programa')
+     sys.exit()
 
-#menu de transações
+# menu de transações
 def transacao(tipo):
-  lista = [{"nome:", nome, "quantidade:", quantidade, "preço:", preco}]
-   dados.append(lista)
-
- if tipo == "compra":
-  nome = input(f'nome do produto de {tipo}:')
-  preco = input(f'preço do produto de {tipo}:')
-  quantidade = input(f"quantidade do produto de {tipo}")
-  lista.append((tipo, nome, preço , quantidade))
- if tipo == "venda":
-  nome = input(f'nome do produto de {tipo}:')
-  preco = input(f'preço do produto de {tipo}:')
-  quantidade = input(f"quantidade do produto de {tipo}")
-  lista.append((tipo, nome, preco, quantidade))
-  print("o saldo total é", total("venda") - total("compra"))
-  
-
-#saldo
-def total(tipo):
-  dados = []
- if tipo == "compra":
-  preco * quantidade
- elif tipo == "venda":
-  preco * quantidade
+    if tipo == "compra":
+        nome = input(f'nome do produto de {tipo}: ')
+        preco = input(f'preço do produto de {tipo}:')
+        quantidade = input(f"quantidade do produto de {tipo}")
+    if tipo == "venda":
+        nome = input(f'nome do produto de {tipo}:')
+        preco = input(f'preço do produto de {tipo}:')
+        quantidade = input(f"quantidade do produto de {tipo}")
+        total = (quantidade(tipo)) * (preco(tipo))
+        lucro =  total("compra") - total("venda")
+        lista = []
+        produto = {tipo, "nome:", nome, "quantidade:", quantidade, "preço:", preco}
+        lista.append(produto)
+        return (lucro)
